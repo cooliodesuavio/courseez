9a7af7f7d2118583e71529f20a015f7a0b0c6e7f
cooliodesuavio <t.brendan.murray@gmail.com>
2014-02-23 19:30:53 -0600

    brendan

--- src/courseezdriver.java
+++ src/courseezdriver.java
@@ -5,7 +5,8 @@ public class courseezdriver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Fuck da World, but not Brendan!!!");
+		System.out.println("Fuck da World, especially Brendan!!!");
+		System.out.println("WTF?!");
 		// TODO Auto-generated method stub
 	}
 }
