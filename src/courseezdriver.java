900a8bccc66c3bd7cb2cc87d7f97f6daeecedde2
cooliodesuavio <t.brendan.murray@gmail.com>
2014-02-23 18:54:56 -0600

    FUCK DA WORLD!!!

--- src/courseezdriver.java
+++ src/courseezdriver.java
@@ -5,7 +5,7 @@ public class courseezdriver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Fuck the World");
+		System.out.println("Fuck da World!!!");
 		// TODO Auto-generated method stub
 
 	}
