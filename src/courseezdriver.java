6b4edcfde0fbea1b56c2abcfe91af8ceb0519549
Kapil Gowru <k.gowru@gmail.com>
2014-02-23 19:33:38 -0600

    added another println

--- src/courseezdriver.java
+++ src/courseezdriver.java
@@ -7,6 +7,7 @@ public class courseezdriver {
 	public static void main(String[] args) {
 		System.out.println("Fuck da World!!...but not Kapil");
 		System.out.println("TESTING");
+		System.out.println("how does this work?");
 	}
 
 }
