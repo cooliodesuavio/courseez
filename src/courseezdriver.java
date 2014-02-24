a4849443f2a76f915c403ebcf05aa760a70891a3
Kapil Gowru <k.gowru@gmail.com>
2014-02-23 18:58:43 -0600

    Changed the main file

--- src/courseezdriver.java
+++ src/courseezdriver.java
@@ -5,7 +5,7 @@ public class courseezdriver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Fuck the World");
+		System.out.println("Fuck da World!!...but not Kapil");
 		// TODO Auto-generated method stub
 
 	}
