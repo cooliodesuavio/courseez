5d7df2e52e1aa60c1d21c1782979704d2945fe5b
cooliodesuavio <t.brendan.murray@gmail.com>
2014-02-24 11:10:35 -0600

    added constructors

--- src/UniqClass.java
+++ src/UniqClass.java
@@ -1,4 +1,17 @@
 
 public class UniqClass {
-
+	int id;
+	String times,days,location,prof,avail;
+	private UniqClass(){
+		
+	}
+	public UniqClass(int id, String times, String days, String location, String prof, String avail)
+	{
+		this.id=id;
+		this.times=times;
+		this.days=days;
+		this.location=location;
+		this.prof=prof;
+		this.avail=avail;
+	}
 }
