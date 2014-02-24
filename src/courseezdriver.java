4e9dd8802d345e6e146cc85fe874b2731941f1c2
Kapil Gowru <k.gowru@gmail.com>
2014-02-23 19:03:37 -0600

    trying this

--- src/courseezdriver.java
+++ src/courseezdriver.java
@@ -6,6 +6,7 @@ public class courseezdriver {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Fuck da World!!...but not Kapil");
+		//https://github.com/cooliodesuavio/courseez
 		// TODO Auto-generated method stub
 
 	}
