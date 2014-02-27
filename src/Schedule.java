20cce6fcf8b16a819fa2c04e6b2a3f81dc3e10ce
cooliodesuavio <t.brendan.murray@gmail.com>
2014-02-26 18:37:08 -0600

    Got the valid permutations to work.  Added documentation(brief, will add more later).

--- src/Schedule.java
+++ src/Schedule.java
@@ -12,13 +12,20 @@ public class Schedule {
 		coursenames=new ArrayList<String>();
 		courseindexes=new ArrayList<Integer>();
 	}
-	public Schedule(boolean[][] time,ArrayList<String> coursenames, ArrayList<Integer> courseindexes){
-		this.time=time;
-		this.coursenames=(ArrayList<String>) coursenames.clone();
-		this.courseindexes=(ArrayList<Integer>) courseindexes.clone();
-		
+	//copy constructor
+	public Schedule(boolean[][] tim,ArrayList<String> coursenames, ArrayList<Integer> courseindexes){
+		time=new boolean[5][96];
+		for(int i=0;i<5;i++){
+			for(int j=0;j<96;j++){
+				this.time[i][j]=tim[i][j];
+			}
+		}
+		this.coursenames=new ArrayList<String>(coursenames);
+		this.courseindexes=new ArrayList<Integer>(courseindexes);	
 	}
+	
 	public boolean addClass(String name, int index, HashMap<String, ArrayList<UniqClass>> coursecatalog){
+		//if the schedule is still valid when the new class is added return true, otherwise return false
 		coursenames.add(name);
 		courseindexes.add(index);
 		UniqClass u = coursecatalog.get(name).get(index);
@@ -29,6 +36,9 @@ public class Schedule {
 	public void clear(){
 		coursenames.clear();
 		courseindexes.clear();
+		for(int i=0;i<5;i++)
+			for(int j=0;j<96;j++)
+				time[i][j]=false;
 	}
 	public ArrayList<Integer> getIndexes(){
 		return courseindexes;
@@ -38,10 +48,12 @@ public class Schedule {
 	}
 	private boolean updateTime(String days, String tim){
 		String[] ind_days = days.split("");
-		String[] times = tim.split(" to ");
+		String[] times = tim.split(" ");
 		int day;
 		int time_ind=0;
+		//for each day that the class is scheduled
 		for(int i =0;i<ind_days.length;i++){
+			//find out the correct index for the day is
 			if(i<ind_days.length-1 && ind_days[i+1].equals("H"))
 				day=3;
 			else{
@@ -59,9 +71,11 @@ public class Schedule {
 				}
 			}
 			if(day!=-1){
+				//grab the start time and end time and correctly alter them for 24 hour time and convert that into an index
+				//NOTE:: the conversion to 24 time will need to be done more correctly somehow
 				int startTime=Integer.parseInt(times[time_ind]);
-				char aorp=times[time_ind+1].charAt(times[time_ind+1].length()-1);
-				int stopTime=Integer.parseInt(times[time_ind+1].substring(0, times[time_ind+1].length()-1));
+				char aorp=times[time_ind+2].charAt(times[time_ind+2].length()-1);
+				int stopTime=Integer.parseInt(times[time_ind+2].substring(0, times[time_ind+2].length()-1));
 				if(aorp=='p'){
 					if(startTime<=700){
 						startTime+=1200;
@@ -72,15 +86,17 @@ public class Schedule {
 				}
 				startTime=((startTime/100) * 4)+((startTime%100)/15);
 				stopTime=((stopTime/100) * 4)+((stopTime%100)/15);
-				for(int j=startTime;j<stopTime-1;j++){
+				//go through time matrix to see if a time slot that the class occupies is already occupied
+				for(int j=startTime;j<stopTime;j++){
 					if(this.time[day][j]==true){
 						return false;
 					}
 					this.time[day][j]=true;
 				}
 			}
+			//move onto the next time listing for class
 			if(ind_days[i].equals(" "))
-				time_ind+=2;
+				time_ind+=3;
 		}
 		return true;
 	}
--- src/courseezdriver.java
+++ src/courseezdriver.java
@@ -13,15 +13,15 @@ public class courseezdriver {
 		coursecatalog=new HashMap<String, ArrayList<UniqClass>>();
 		ArrayList<UniqClass> listing = new ArrayList<UniqClass>();
 		//Example of an addition of class to the HashMap
-		listing.add(new UniqClass(17020,"900 to 1000a to 930 to 1230p", "MWF T","CPE 2.212 ENS 307", "JULIEN, C", "open"));
-		listing.add(new UniqClass(17025,"900 to 1000a to 1230 to 330p", "MWF T","CPE 2.212 ENS 307", "JULIEN, C", "open"));
-		listing.add(new UniqClass(17030,"900 to 1000a to 330 to 630p", "MWF T","CPE 2.212 ENS 307", "JULIEN, C", "open"));
-		listing.add(new UniqClass(17035,"900 to 1000a to 630 to 930p", "MWF T","CPE 2.212 ENS 307", "JULIEN, C", "open"));
+		listing.add(new UniqClass(17020,"900 to 1000a 930 to 1230p", "MWF T","CPE 2.212 ENS 307", "JULIEN, C", "open"));
+		listing.add(new UniqClass(17025,"900 to 1000a 1230 to 330p", "MWF T","CPE 2.212 ENS 307", "JULIEN, C", "open"));
+		listing.add(new UniqClass(17030,"900 to 1000a 330 to 630p", "MWF T","CPE 2.212 ENS 307", "JULIEN, C", "open"));
+		listing.add(new UniqClass(17035,"900 to 1000a 630 to 930p", "MWF T","CPE 2.212 ENS 307", "JULIEN, C", "open"));
 		coursecatalog.put("EE461L", (ArrayList<UniqClass>) listing.clone());
 		listing.clear();
-		listing.add(new UniqClass(17075,"1200 to 100p to 1200 to 300p", "MW F","RLM 5.104 ENS 212", "HALLOCK, G", "open"));
-		listing.add(new UniqClass(17080,"1200 to 100p to 300 to 600p", "MW W","RLM 5.104 ENS 212", "HALLOCK, G", "open"));
-		listing.add(new UniqClass(17085,"1200 to 100p to 630 to 930p", "MW W","RLM 5.104 ENS 212", "HALLOCK, G", "open"));
+		listing.add(new UniqClass(17075,"1200 to 100p 1200 to 300p", "MW F","RLM 5.104 ENS 212", "HALLOCK, G", "open"));
+		listing.add(new UniqClass(17080,"1200 to 100p 300 to 600p", "MW W","RLM 5.104 ENS 212", "HALLOCK, G", "open"));
+		listing.add(new UniqClass(17085,"1200 to 100p 630 to 930p", "MW W","RLM 5.104 ENS 212", "HALLOCK, G", "open"));
 		coursecatalog.put("EE364D", (ArrayList<UniqClass>) listing.clone());
 		listing.clear();
 		listing.add(new UniqClass(17040,"130 to 300p", "MW","WAG 214", "ZHOU, Y", "open"));
@@ -34,14 +34,18 @@ public class courseezdriver {
 		listing.add(new UniqClass(5385,"200 to 330p", "TTH","UTC 2.102A", "MILLER, H", "open"));
 		coursecatalog.put("MKT320F", (ArrayList<UniqClass>) listing.clone());
 		listing.clear();
-		for(String course : coursecatalog.keySet()){
+		/*for(String course : coursecatalog.keySet()){
 			System.out.println(course+":");
 			for(UniqClass uclass: coursecatalog.get(course)){
 				System.out.println("\t"+uclass.id);
 			}
-		}
-		String[] choices = {"EE364D","EE361Q"};//,"EE361Q","MKT320F","EE360C"};
+		}*/
+		
+		
+		String[] choices = {"EE364D","EE361Q","EE461L","MKT320F","EE360C"};
 		ArrayList<Schedule> perms = allPermutations(choices,null,0);
+		
+		
 		for(Schedule sc : perms){
 			System.out.println(sc.getNames()+","+sc.getIndexes());
 		}
@@ -53,19 +57,21 @@ public class courseezdriver {
 			return allPossible;
 		int size = coursecatalog.get(courses[index]).size();
 		Schedule tmp = new Schedule();
+		//if no schedules have been added, add a permutation for each class
 		if(allPossible==null){
 			allPossible = new ArrayList<Schedule>(); 	
 			for(int i =0; i<size;i++){
-				tmp.addClass(courses[0], i,coursecatalog);
-				allPossible.add(new Schedule(tmp.time,tmp.coursenames,tmp.courseindexes));
+				if(tmp.addClass(courses[0], i,coursecatalog))
+					allPossible.add(new Schedule(tmp.time,tmp.coursenames,tmp.courseindexes));
 				tmp.clear();
 			}
 			return allPermutations(courses,allPossible,1);
 		}
 		ArrayList<Schedule> temp =new ArrayList<Schedule>();
+		//for each already valid schedule, add a new permutation that includes the new class
 		for(Schedule sc : allPossible){
 			for(int j=0;j<size;j++){
-					tmp=new Schedule(sc.time,sc.coursenames,sc.courseindexes);
+					tmp = new Schedule(sc.time,sc.coursenames,sc.courseindexes);
 					if(tmp.addClass(courses[index], j,coursecatalog))
 						temp.add(new Schedule(tmp.time,tmp.coursenames,tmp.courseindexes));
 					tmp.clear();
